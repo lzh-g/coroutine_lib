4f8a732349008b0deecd36941b6b45d7867633be
lzh-g <2232284193@qq.com>
2025-03-31 00:11:40 +0800

    添加调度器模块声明

--- fiber/fiber.cpp
+++ fiber/fiber.cpp
@@ -1,43 +1,210 @@
 #include "fiber.h"
 
-sylar::Fiber::Fiber(std::function<void()> cb, size_t stacksize, bool run_in_scheduler)
-{
-}
+static bool debug = false;
 
-sylar::Fiber::~Fiber()
+namespace sylar
 {
-}
+    // 当前线程上的协程控制信息
 
-void sylar::Fiber::reset(std::function<void()> cb)
-{
-}
+    // 正在运行的协程
+    static thread_local Fiber *t_fiber = nullptr;
+    // 主协程
+    static thread_local std::shared_ptr<Fiber> t_thread_fiber = nullptr;
+    // 调度协程
+    static thread_local Fiber *t_scheduler_fiber = nullptr;
 
-void sylar::Fiber::resume()
-{
-}
+    // 全局协程id计数器
+    static std::atomic<uint64_t> s_fiber_id{0};
+    // 活跃协程数量计数器
+    static std::atomic<uint64_t> s_fiber_count{0};
 
-void sylar::Fiber::yield()
-{
-}
+    Fiber::Fiber(std::function<void()> cb, size_t stacksize, bool run_in_scheduler) : m_cb(cb), m_runInScheduler(run_in_scheduler)
+    {
+        // 初始化状态
+        m_state = READY;
 
-void sylar::Fiber::SetThis(Fiber *f)
-{
-}
+        // 分配协程栈空间
+        m_staciSize = stacksize ? stacksize : 128000;
+        m_stack = malloc(m_staciSize);
 
-std::shared_ptr<Fiber> sylar::Fiber::GetThis()
-{
-    return std::shared_ptr<Fiber>();
-}
+        if (getcontext(&m_ctx))
+        {
+            std::cerr << "Fiber(std::function<void()> cb, size_t stackeize, bool run_in_scheduler) failed" << std::endl;
+            pthread_exit(nullptr);
+        }
 
-void sylar::Fiber::SetSchedulerFiber(Fiber *f)
-{
-}
+        // 没设置后继上下文，运行完MainFunc后协程推出，会调用依次yield返回主协程
+        m_ctx.uc_link = nullptr;
+        m_ctx.uc_stack.ss_sp = m_stack;
+        m_ctx.uc_stack.ss_size = m_staciSize;
+        makecontext(&m_ctx, &Fiber::MainFunc, 0);
 
-uint64_t sylar::Fiber::GetFiberId()
-{
-    return 0;
-}
+        m_id = s_fiber_id++;
+        s_fiber_count++;
+        if (debug)
+        {
+            std::cout << "Fiber(): child id = " << m_id << std::endl;
+        }
+    }
 
-void sylar::Fiber::MainFunc()
-{
-}
+    Fiber::~Fiber()
+    {
+        // 减少活跃协程计数器
+        s_fiber_count--;
+
+        if (m_stack)
+        {
+            free(m_stack);
+        }
+        if (debug)
+        {
+            std::cout << "~Fiber(): id = " << m_id << std::endl;
+        }
+    }
+
+    void Fiber::reset(std::function<void()> cb)
+    {
+        assert(m_stack != nullptr && m_state == TERM);
+
+        m_state = READY;
+        m_cb = cb;
+
+        if (getcontext(&m_ctx))
+        {
+            std::cerr << "reset() failed" << std::endl;
+            pthread_exit(nullptr);
+        }
+
+        m_ctx.uc_link = nullptr;
+        m_ctx.uc_stack.ss_sp = m_stack;
+        m_ctx.uc_stack.ss_size = m_staciSize;
+        makecontext(&m_ctx, &Fiber::MainFunc, 0);
+    }
+
+    void Fiber::resume()
+    {
+        assert(m_state == READY);
+
+        m_state = RUNNING;
+
+        if (m_runInScheduler)
+        {
+            SetThis(this);
+            if (swapcontext(&(t_scheduler_fiber->m_ctx), &m_ctx))
+            {
+                std::cerr << "resume() to t_scheduler_fiber failed" << std::endl;
+                pthread_exit(nullptr);
+            }
+        }
+        else
+        {
+            SetThis(this);
+            if (swapcontext(&(t_thread_fiber->m_ctx), &m_ctx))
+            {
+                std::cerr << "resume() to t_thread_fiber failed" << std::endl;
+                pthread_exit(nullptr);
+            }
+        }
+    }
+
+    void Fiber::yield()
+    {
+        assert(m_state == RUNNING || m_state == TERM);
+
+        if (m_state != TERM)
+        {
+            m_state = READY;
+        }
+
+        if (m_runInScheduler)
+        {
+            SetThis(t_scheduler_fiber);
+            if (swapcontext(&m_ctx, &(t_scheduler_fiber->m_ctx)))
+            {
+                std::cerr << "yield() to t_scheduler_fiber failed" << std::endl;
+                pthread_exit(nullptr);
+            }
+        }
+        else
+        {
+            SetThis(t_thread_fiber.get());
+            if (swapcontext(&m_ctx, &(t_thread_fiber->m_ctx)))
+            {
+                std::cerr << "yield() to t_thread_fiber failed" << std::endl;
+                pthread_exit(nullptr);
+            }
+        }
+    }
+
+    void Fiber::SetThis(Fiber *f)
+    {
+        t_fiber = f;
+    }
+
+    // 首先运行该函数创建主协程
+    std::shared_ptr<Fiber> Fiber::GetThis()
+    {
+        if (t_fiber)
+        {
+            return t_fiber->shared_from_this();
+        }
+
+        std::shared_ptr<Fiber> main_fiber(new Fiber());
+        t_thread_fiber = main_fiber;
+        t_scheduler_fiber = main_fiber.get(); // 除非主动设置 主协程默认为调度协程
+
+        // 用于判断t_fiber是否等于main_fiber，是：继续执行；否：程序终止
+        assert(t_fiber == main_fiber.get());
+        return t_fiber->shared_from_this();
+    }
+
+    void Fiber::SetSchedulerFiber(Fiber *f)
+    {
+        t_scheduler_fiber = f;
+    }
+
+    uint64_t Fiber::GetFiberId()
+    {
+        if (t_fiber)
+        {
+            return t_fiber->getId();
+        }
+        // (uint64_t)-1会转化为UINT64_MAX，用来表示错误的情况
+        return (uint64_t)-1;
+    }
+
+    void Fiber::MainFunc()
+    {
+        std::shared_ptr<Fiber> curr = GetThis();
+        assert(curr != nullptr);
+
+        curr->m_cb();
+        curr->m_cb = nullptr;
+        curr->m_state = TERM;
+
+        // 运行完毕 -> 让出执行权
+        auto raw_ptr = curr.get();
+        curr.reset(); // shared_ptr中的方法，释放当前管理的对象，只能指针引用计数-1
+        raw_ptr->yield();
+    }
+
+    Fiber::Fiber()
+    {
+        SetThis(this);
+        m_state = RUNNING;
+
+        if (getcontext(&m_ctx))
+        {
+            std::cerr << "Fiber() failed" << std::endl;
+            pthread_exit(nullptr);
+        }
+
+        m_id = s_fiber_id++;
+        s_fiber_count++;
+        if (debug)
+        {
+            std::cout << "Fiber(): main id = " << m_id << std::endl;
+        }
+    }
+
+} // namespace sylar
--- fiber/fiber.h
+++ fiber/fiber.h
@@ -24,13 +24,15 @@ namespace sylar
         };
 
     public:
+        // 创建一个新协程，初始化回调函数，栈的大小和状态，分配栈空间，并通过make修改上下文，当set或swap激活ucontext_t m_ctx上下文时会执行make第二个参数的函数
         Fiber(std::function<void()> cb, size_t stacksize = 0, bool run_in_scheduler = true);
+
         ~Fiber();
 
         // 重用一个协程
         void reset(std::function<void()> cb);
 
-        // 任务协程恢复执行
+        // 任务协程恢复执行，重置协程的状态为RUNNING，恢复协程的执行。若m_runInScheduler为true，则将上下文切换到调度协程，否则切换到主协程
         void resume();
 
         // 任务协程让出执行权
